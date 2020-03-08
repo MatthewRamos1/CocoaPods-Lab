43434dfea7b6a51321f3ea4bb2e509b9a54cc9bc
Matthew Ramos <mmramos309@gmail.com>
2020-03-08 18:52:40 -0400

    Set up API Client

---
+++ CocoaPods-Lab/UserAPIClient.swift
@@ -0,0 +1,35 @@
+//
+//  UserAPIClient.swift
+//  CocoaPods-Lab
+//
+//  Created by Matthew Ramos on 3/8/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct UserAPIClient {
+    static func fetchUsers(completion: @escaping (AFResult<[User]>) -> ()) {
+        let endpointURLString = "https://randomuser.me/api/?results=20"
+        
+        guard let url = URL(string: endpointURLString) else {
+            return
+        }
+        
+        AF.request(url).response { (response) in
+            
+            if let error = response.error {
+                completion(.failure(error))
+            } else if let data = response.data {
+                do{
+                    let results = try JSONDecoder().decode(ResultsWrapper.self, from: data)
+                    let users = results.results
+                    completion(.success(users))
+                } catch {
+                    print("Couldn't decode data")
+                }
+            }
+        }
+    }
+}
